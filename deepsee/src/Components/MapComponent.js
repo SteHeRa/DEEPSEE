03a2a2b8f8d1b2d22bf1e1536230eab674d4780d
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-09-23 19:56:18 +0100

    feat: dive site pop-ups rendering

--- deepsee/src/Components/MapComponent.js
+++ deepsee/src/Components/MapComponent.js
@@ -3,46 +3,55 @@ import mapboxgl from 'mapbox-gl';
 
 function Map () {
 
-  const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
 
-  // const [lng, setLng] = useState(119.654983);
-  // const [lat, setLat] = useState(-8.61207);
-  // const [zoom, setZoom] = useState(10.22);
+  const [lng, setLng] = useState(119.5872);
+  const [lat, setLat] = useState(-8.6057);
+  const [zoom, setZoom] = useState(10.22);
 
   useEffect( () => {
+
     mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlaGVyYSIsImEiOiJja2ZmOTYxNTgwY2sxMnJvM3R3bGhwbW05In0.nzQVNouROweGSb7p6dw2cA';
-    const initializeMap = ({ setMap, mapContainer }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: 'mapbox://styles/stehera/ckffagb3k12jh1api1n2lau45',
-        center: [119.654983, -8.61207],
-        zoom: 10.22
+        center: [lng, lat],
+        zoom: zoom
       });
 
-      map.on('load', () => {
-        setMap(map);
-        map.resize();
-      });
-    };
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4))
+      setLat(map.getCenter().lat.toFixed(4))
+      setZoom(map.getZoom().toFixed(4))
+    })
 
-    if (!map) initializeMap({ setMap, mapContainer });
+    map.on('click', function(e) {
+      var features = map.queryRenderedFeatures(e.point, {
+        layers: ['komodo-dive-sites'] // replace this with the name of the layer
+    });
 
-    // map.on('move', () => {
-    //   setLng(map.getCenter().lng.toFixed(4))
-    //   setLat(map.getCenter().lat.toFixed(4))
-    //   setZoom(map.getZoom().toFixed(4))
-    // })
+    if (!features.length) {
+      return;
+    }
 
-  }, [map]);
+    var feature = features[0];
+
+    var popup = new mapboxgl.Popup({ offset: [0, -15] })
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML('<h3>' + feature.properties.title + '</h3>')
+      .addTo(map);
+    });
+
+  }, []);
+
 
     return (
       <div>
-        {/* <div className='sidebarStyle'>
+        <div className='sidebar-style'>
           <div>Longitude: {lng} | Latitude:
             {lat} | Zoom: {zoom}
           </div>
-        </div> */}
+        </div>
         <div ref={el => mapContainer.current = el}
           className="map-container"/>
       </div>
