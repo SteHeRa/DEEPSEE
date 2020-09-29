871cd5a1a3dd096814fbb16e229ac7dd7d10c1f8
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-09-29 10:09:27 +0100

    if no log book is empty add dive button in center of screen

--- deepsee/src/Components/LogBookComponent.js
+++ deepsee/src/Components/LogBookComponent.js
@@ -1,49 +1,53 @@
-import React, { useState, useEffect }from 'react';
-import ReactDOM from 'react-dom';
-import ApiClient from '../services/ApiClient';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import ApiClient from "../services/ApiClient";
 
-import LogList from './LogListComponent';
-import LogFormModal from './LogFormModal';
-import DiveDetailsModal from './DiveDetailsModal';
+import LogList from "./LogListComponent";
+import LogFormModal from "./LogFormModal";
+import DiveDetailsModal from "./DiveDetailsModal";
 
-function Log () {
+function Log() {
+  const [logData, setLogData] = useState([]);
 
-  const [ logData, setLogData ] = useState([])
-
-  useEffect( () => makeLogList(), []) //TODO refactor so log list is updated every time an item is added
+  useEffect(() => makeLogList(), []); //TODO refactor so log list is updated every time an item is added
 
   function makeLogList() {
-    ApiClient.getLogs()
-      .then( logList => {
-        if (logList[0] !== undefined) {
-          logList.sort((a, b) => b.date - a.date); //TODO sort logs made on the same day by most recently posted
-          setLogData(logList);
-        } else setLogData([]);
-      });
+    ApiClient.getLogs().then((logList) => {
+      if (logList[0] !== undefined) {
+        logList.sort((a, b) => b.date - a.date); //TODO sort logs made on the same day by most recently posted
+        setLogData(logList);
+      } else setLogData([]);
+    });
   }
 
-  function openLogForm () {
-    const modalRoot = document.getElementById('modal-root');
-    ReactDOM.render( //might be better to use ReactDOM.createPortal
-      <LogFormModal makeLogList={() => makeLogList()} closeModal={closeModal}/>,
+  function openLogForm() {
+    const modalRoot = document.getElementById("modal-root");
+    ReactDOM.render(
+      //might be better to use ReactDOM.createPortal
+      <LogFormModal
+        makeLogList={() => makeLogList()}
+        closeModal={closeModal}
+      />,
       modalRoot
-      )
+    );
   }
 
-  function openDetails (dive) {
-    const modalRoot = document.getElementById('modal-root');
-    ReactDOM.render( //might be better to use ReactDOM.createPortal
-      <DiveDetailsModal dive={dive} closeModal={closeModal}/>,
+  function openDetails(dive) {
+    const modalRoot = document.getElementById("modal-root");
+    ReactDOM.render(
+      //might be better to use ReactDOM.createPortal
+      <DiveDetailsModal dive={dive} closeModal={closeModal} />,
       modalRoot
-      )
+    );
   }
 
-  function closeModal () {
-    const modalRoot = document.getElementById('modal-root');
-    ReactDOM.render( //might be better to use ReactDOM.createPortal
+  function closeModal() {
+    const modalRoot = document.getElementById("modal-root");
+    ReactDOM.render(
+      //might be better to use ReactDOM.createPortal
       null,
       modalRoot
-      )
+    );
   }
 
   return (
@@ -52,11 +56,15 @@ function Log () {
       <div className="log-book-content">
         <div id="add-dive-button" onClick={() => openLogForm()}>
           <div id="add-dive-button-plus">+</div>
-          </div>
-        <LogList log={logData} openDetails={(dive) => openDetails(dive)}/>
+        </div>
+        <div>
+          {logData === [] ? (
+            <LogList log={logData} openDetails={(dive) => openDetails(dive)} />
+          ) : null}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Log;
