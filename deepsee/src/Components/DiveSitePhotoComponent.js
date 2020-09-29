68ad0c7d0df794421591d8a9e3feec0c2dc26557
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-09-29 17:34:54 +0100

    added data to dive-site-photo component

--- deepsee/src/Components/DiveSitePhotoComponent.js
+++ deepsee/src/Components/DiveSitePhotoComponent.js
@@ -8,6 +8,12 @@ function DiveSitePhoto({ photo }) {
         src={photo.URL}
         alt="Image from dive site"
       />
+      <div className="photo-data">
+        <div className="photo-conditions">
+          Visibility: {photo.visibility} m - Temperature: {photo.temp}&deg;C
+        </div>
+        <div className="photo-date">{new Date(photo.date).toDateString()}</div>
+      </div>
     </div>
   );
 }
